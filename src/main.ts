2857cf997f3e6e3b53c0afd444c4cc3a5d9c089f
srslafazan <srslafazan@gmail.com>
2023-01-02 19:29:54 -0800

    Added docs file

---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import path from 'path'
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import redoc from 'redoc-express'
+
+const prisma = new PrismaClient()
+
+export async function main() {
+  console.log('Hello World')
+  await prisma.$connect()
+  console.log('Prisma Connected')
+  dotenv.config()
+
+  const app: Express = express()
+  const port = process.env.PORT
+
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  // app.get('/docs/swagger.json', (req, res) => {
+  //   res.sendFile('swagger.json', { root: '.' })
+  // })
+
+  app.get(
+    '/docs',
+    redoc({
+      title: 'API Docs',
+      specUrl: '/swagger.json',
+    }),
+  )
+
+  app.get('/version', (req: Request, res: Response) => {
+    return res.send({
+      version: require('../package.json').version,
+    })
+  })
+
+  app.get('/', (req: Request, res: Response) => {
+    return res.send('Express + TypeScript Server')
+  })
+
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+  })
+  // ...
+  return true
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
+// console.log('Hello World')
+// // await prisma.$connect()
+// console.log('Prisma Connected')
+// dotenv.config()
+
+// const app: Express = express()
+// const port = process.env.PORT
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Express + TypeScript Server')
+// })
+
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+// })
