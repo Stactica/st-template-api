d6186cd2aaa1955340f02ccb041a781465ceb7ec
srslafazan <srslafazan@gmail.com>
2023-01-02 19:39:38 -0800

    Turn off swagger-autogen in production

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-const swaggerAutogen = require('swagger-autogen')
-const outputFile = './public/swagger.json'
-const endpointsFiles = ['./main.ts']
-const config = {}
+if (process.env.NODE_ENV === 'development') {
+  const swaggerAutogen = require('swagger-autogen')
+  const outputFile = './public/swagger.json'
+  const endpointsFiles = ['./main.ts']
+  const config = {}
 
-swaggerAutogen()(outputFile, endpointsFiles, config).then(async () => {
-  await import('./main')
-})
+  swaggerAutogen()(outputFile, endpointsFiles, config).then(async () => {
+    await import('./main')
+  })
+} else {
+  import('./main')
+}
